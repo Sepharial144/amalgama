9716d9d7ca80a2858fae8d2479f93cc41c4bf2b0
Sepharial144 <ivaschenkoda144@gmail.com>
2024-08-21 12:23:33 +0000

    tcp fix

--- tools/x64compile.sh
+++ tools/x64compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug -DTARGET=$1 && cmake --build build -j4
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=Debug && cmake --build build -j4
---
+++ utils/tcp/source/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdint>
+
+int32_t main(int argc, char* argv[]) {
+    std::cout << "Hello tcp" << std::endl;
+    return 0;
+}
